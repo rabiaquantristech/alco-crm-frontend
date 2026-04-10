8ed68b5d4420956a58179722b10b3c5fc9d2f225
rabiaquantristech <rabiaquantristech@gmail.com>
2026-04-10 10:14:13 +0500

    fix: loader on suspense

--- src/app/auth/callback/page.tsx
+++ src/app/auth/callback/page.tsx
@@ -5,6 +5,7 @@ import { useRouter, useSearchParams } from "next/navigation";
 import { useAppDispatch } from "@/store/hooks";
 import { setCredentials } from "@/store/authSlice";
 import toast from "react-hot-toast";
+import Loader from "@/app/component/loader/Loader";
 
 function AuthCallbackInner() {
   const router = useRouter();
@@ -58,51 +59,8 @@ function AuthCallbackInner() {
 
 export default function AuthCallback() {
   return (
-    <Suspense fallback={
-      <div className="min-h-screen flex items-center justify-center">
-        <p className="text-gray-500">Loading...</p>
-      </div>
-    }>
+    <Suspense fallback={<Loader />}>
       <AuthCallbackInner />
     </Suspense>
   );
 }
-
-// "use client";
-// import { useEffect } from "react";
-// import { useRouter, useSearchParams } from "next/navigation";
-// import { useAppDispatch } from "@/store/hooks";
-// import { setCredentials } from "@/store/authSlice";
-// import toast from "react-hot-toast";
-
-// export default function AuthCallback() {
-//   const router = useRouter();
-//   const searchParams = useSearchParams();
-//   const dispatch = useAppDispatch();
-
-//   useEffect(() => {
-//     const token = searchParams.get("token");
-//     const userStr = searchParams.get("user");
-
-//     if (token && userStr) {
-//       try {
-//         const user = JSON.parse(decodeURIComponent(userStr));
-//         dispatch(setCredentials({ user, token }));
-//         toast.success("Login successful! 🎉");
-//         router.push("/dashboard");
-//       } catch (err) {
-//         toast.error("Login failed!");
-//         router.push("/login");
-//       }
-//     } else {
-//       toast.error("Auth failed!");
-//       router.push("/login");
-//     }
-//   }, []);
-
-//   return (
-//     <div className="min-h-screen flex items-center justify-center">
-//       <p className="text-gray-500">Logging you in...</p>
-//     </div>
-//   );
-// }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,10 @@
 import { Suspense } from "react";
 import LoginClient from "./loginClient";
+import Loader from "../component/loader/Loader";
 
 export default function LoginPage() {
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<Loader />}>
       <LoginClient />
     </Suspense>
   );
